6c0895d82490694fcf4aadca8cb160ee1819f09a
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-07 15:02:40 +0530

    update forms to ModelForms

--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,7 @@ from catalog.models import Book, Author, BookInstance, Genre
 from django.contrib.auth.mixins import LoginRequiredMixin
 import datetime
 from django.urls import reverse
-from catalog.forms import RenewBookForm
+from catalog.forms import RenewBookModelForm
 from django.contrib.auth.decorators import login_required, permission_required
 
 
@@ -106,3 +106,20 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from catalog.models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
